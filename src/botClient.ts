86c6a6998c985b703bce99dec6dec50593faf668
eatradish <sakiiily@aosc.io>
2019-08-26 08:30:52 +0800

    botClient: fix logic

--- src/botClient.ts
+++ src/botClient.ts
@@ -5,19 +5,21 @@ import * as readline from 'readline';
 
 const main = async (): Promise<void> => {
     const bot = new Bot(token.botClient);
-    bot.on(/.*/, (msg: BotAPI.BotGetUpdatesMessage | BotAPI.BotGetUpdatesChannelPost) => {
+    bot.on(/.*/, (msg: BotAPI.BotGetUpdatesResultMessage | BotAPI.BotGetUpdatesResultChannelPost) => {
         let a: string;
         const type = msg.chat.type;
         if (type == 'group' || type == 'supergroup' || type == 'private') {
-            msg = msg as BotAPI.BotGetUpdatesMessage;
+            msg = msg as BotAPI.BotGetUpdatesResultMessage;
             if (type == 'group' || type == 'supergroup') a = msg.chat.title;
             else a = 'private';
-            console.log('> (' + a + ') ' + msg.from.username + ' (' +
+            if (msg.text) {
+                console.log('> (' + a + ') ' + msg.from.username + ' (' +
                 msg.from.first_name + ' ' + msg.from.last_name + '): ' + msg.text);
+            }
         }
         else {
-            msg = msg as BotAPI.BotGetUpdatesChannelPost;
-            console.log('> (' + msg.chat.title + '): ' + msg.text);
+            msg = msg as BotAPI.BotGetUpdatesResultChannelPost;
+            if (msg.text) console.log('> (' + msg.chat.title + '): ' + msg.text);
         }
     });
     bot.listen();
@@ -35,10 +37,10 @@ const main = async (): Promise<void> => {
             const id = Number(data[0])
             const text = data.splice(-1, 1).join('');
             if (id !== undefined && id !== NaN) result = await bot.sendMessage(id, text);
-            if (result !== undefined) console.log('You: ' + result.result.text);
+            if (result !== undefined) console.log('to ' + map.get(id) + ': '+ result.result.text);
         });
     }
-    const map = await bot.getUserList()
+    const map = await bot.getUserList();
     send(map);
 }
 
